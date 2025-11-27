02b753df35fa32fa0c3cb1d898488d89ec82bb4b
designervj <vijendrachoudhary3344@gmail.com>
2025-11-27 21:25:50 +0530

    design code

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,8 @@
 
 import Hero from "@/components/sections/Hero";
 
-import AboutStrip from "@/components/sections/AboutStrip";
-// import ProductTabsGrid, { Product } from "@/components/sections/ProductTabsGrid";
-import AboutKarloBan from "@/components/sections/AboutKarloBan";
-import Testimonials from "@/components/sections/Testimonials";
-import OurWork from "@/components/sections/OurWork";
-import ContactUs from "@/components/sections/ContactUs";
-import NewsSection from "@/components/sections/NewsSection";
+
+
 
 
 export default function HomePage() {
@@ -15,9 +10,7 @@ export default function HomePage() {
     <main>
       <Hero />
       {/* <AboutKarloBan /> */}
-      <AboutStrip/>
-      {/* <OurWork/> */}
-      <Testimonials />
+
 
    {/* <NewsSection /> */}
         {/* <ContactUs/> */}
